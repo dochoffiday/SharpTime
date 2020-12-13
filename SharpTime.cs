8abfa05b176347e0d90b34656dcc251276993ac3
AJ Hoffer <aj@hoffer.fm>
2020-12-13 16:57:56 -0500

    Initial commit

---
+++ SharpTime.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SharpTime
+{
+    public static class SharpTime
+    {
+        [ThreadStatic]
+        private static DateTime? _dateTimeUtc;
+
+        public static DateTime UtcNow
+        {
+            get
+            {
+                if (_dateTimeUtc.HasValue)
+                {
+                    return _dateTimeUtc.Value;
+                }
+
+                return DateTime.UtcNow;
+            }
+        }
+
+        public static IDisposable UseSpecificDateTimeUtc(DateTime dateTimeUtc)
+        {
+            if (_dateTimeUtc.HasValue) throw new InvalidOperationException("SystemTime is already locked");
+
+            _dateTimeUtc = dateTimeUtc;
+
+            return new LockedDateTimeUtc();
+        }
+
+        private class LockedDateTimeUtc : IDisposable
+        {
+            public void Dispose()
+            {
+                _dateTimeUtc = null;
+
+                GC.SuppressFinalize(this);
+            }
+        }
+    }
+}
